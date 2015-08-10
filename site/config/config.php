b57cd704dce605d2acd39a75a7dbc1f6d7887df6
Jonas Döbertin <hello@jd-powered.net>
2015-08-10 10:23:54 +0200

    Sitemap rules

--- site/config/config.php
+++ site/config/config.php
@@ -6,10 +6,36 @@ c::set('markdown.extra', true);
 
 c::set('routes', [
 
+    /*
+        THEME
+        =====
+        
+     */
     [
         'pattern' => 'api/theme/custom.min.css',
         'action'  => function() {
             return new Response(Theme::trumps(), 'css', 200);
+        },
+    ],
+
+    /*
+        SITEMAP
+        =======
+        1. Reroute calls to "/sitemap" to "/sitemap.xml"
+        2. Return "/sitemap" when fetching "sitemap.xml"
+
+        Seems stupid, but from a SEO perspective it's reasonable.
+     */
+    [
+        'pattern' => 'sitemap',
+        'action'  => function() {
+            go('sitemap.xml');
+        },
+    ],
+    [
+        'pattern' => 'sitemap.xml',
+        'action'  => function() {
+            return site()->visit('sitemap');
         },
     ],
 
