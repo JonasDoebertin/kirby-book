999574d0f9aba5bb585c4030faa633e5c0ba34a9
Jonas Döbertin <hello@jd-powered.net>
2015-10-13 14:34:51 +0200

    #1: Add visuals

--- site/snippets/book.php
+++ site/snippets/book.php
@@ -3,4 +3,6 @@
         <?php snippet('book/header') ?>
         <?php snippet('book/page') ?>
     </div>
+    <a class="book__previous" href="#" aria-role="navigation" aria-label=""><i class="fa fa-angle-left"></i></a>
+    <a class="book__next" href="#" aria-role="navigation" aria-label=""><i class="fa fa-angle-right"></i></a>
 </section>
