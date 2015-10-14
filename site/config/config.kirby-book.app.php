b59946edcc1daa1254fda3bb13907a83b033ff37
Jonas Döbertin <hello@jd-powered.net>
2015-10-14 11:22:17 +0200

    Create local config

---
+++ site/config/config.kirby-book.app.php
@@ -0,0 +1,14 @@
+<?php
+/*
+|-------------------------------------------------------------------------------
+| Debugging
+|-------------------------------------------------------------------------------
+|
+| Please add your license key, which you've received via email after purchasing
+| Kirby on http://getkirby.com/buy. It is not permitted to run a public website
+| without a valid license key. Please read the End User License Agreement for
+| more information: http://getkirby.com/license
+|
+*/
+
+c::set('debug', true);
